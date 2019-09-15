e33515449bd809733719ac9f4ecb9110794acb2f
Jean <jpmejicanos@ufm.edu>
2019-09-15 05:31:34 -0600

    Update soup.py
    get links

--- soup.py
+++ soup.py
@@ -5,11 +5,11 @@ import requests,sys,csv,json
 url="http://ufm.edu/Portal"
 # Make a GET request to fetch the raw HTML content
 try:
-    html_content = requests.get(url).text
+    request_content = requests.get(url)
 except:
     print(f"unable to get {url}")
     sys.exit(1)
-
+html_content = request_content.text
 # Parse the html content, this is the Magic ;)
 soup = BeautifulSoup(html_content, "html.parser")
 
@@ -18,7 +18,15 @@ soup = BeautifulSoup(html_content, "html.parser")
 
 print(soup.title)
 print(soup.title.string)
+input()
+# for div in soup.find_all("div"):
+#     print(div)
+#     print("--------------------------")
+def get_links(content, soup):
+    links = []
+    for tag in soup.find_all('a', href=True):
+        links.append(tag['href'])
+    return links
 
-for div in soup.find_all("div"):
-    print(div)
-    print("--------------------------")
+
+print(get_links(html_content,soup))
